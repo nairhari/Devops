6f66f6c8609509400c360428e7f0278c5cf9d230
Hari Nair <dklogictech@outlook.com>
2019-12-02 10:44:09 +0000

    first commit

---
+++ msbuild-example/MsbuildLibrary/LibraryExample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MsbuildLibrary
+{
+    public class LibraryExample
+    {
+        public static string HelloWorld()
+        {
+            return "Hello world1";
+            
+        }
+    }
+}
